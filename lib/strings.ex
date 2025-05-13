7e04a67d251bd79c3e5922794615af629a13fc92
Julian Gomez <julngomz@gmail.com>
2025-05-12 21:46:05 -0500

    initial commit

---
+++ lib/strings.ex
@@ -0,0 +1,18 @@
+defmodule Strings do
+  @moduledoc """
+  Documentation for `Strings`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Strings.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
